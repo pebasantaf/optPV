aa5e91703814cac94997619aa65d53aaabc045ae
pebasantaf <pedrobasantafranco@outlook.com>
2020-03-06 15:53:22 +0100

    Initial commit for file that will whole all the solar energy related functions

---
+++ solar_functions.py
@@ -0,0 +1,10 @@
+# SOLAR ENERGY IMPORT MODULE
+#   This function is aimed at developing all the functions that will be dealing with processing of the solar data. To get
+# a better overview, check the first issue in Github.
+
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
