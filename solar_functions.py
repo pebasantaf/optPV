d2e7792b61b1a3b8f3042edcea787a782ee5c576
pebasantaf <pedrobasantafranco@outlook.com>
2020-06-09 17:29:46 +0200

    function for calculating GHI from pvgis data

--- solar_functions.py
+++ solar_functions.py
@@ -3,14 +3,28 @@
 # a better overview, check the first issue in Github.
 
 import glob
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pvlib
+
+def GHIcalc(Gb, Gd, N, lat, sh):
+
+    omega = ((2*math.pi)/24) * sh
+
+    # calculate solar declination
+    delta = 23.45*math.sin((360/365)*(284 + N))
+
+    # clculate the cost of the zenith angle
+    cosz = math.cos(omega)*cos + math.cos(delta) + math.cos(lat) + math.sin(delta) + math.sin(lat)
+
+    GHI = Gb + Gd*cosz
+    return GHI
+
+
+def getOrientedIrradiation(GHI, location, orientation, angle, panel, type):
 
 
 
-def getOrientedIrradiation():
-
-    
-
-    
+    return
