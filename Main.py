bab45f93f4f0907ec5014bf710a257fdb5c8737e
pebasantaf <pedrobasantafranco@outlook.com>
2021-12-20 21:10:46 +0100

    Modifications:
    * Main script created to interact with funtions
    * init file for importing functions
    * functions for read directory and import deman as DF included

---
+++ Main.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import pandas as pd
+from demand_data  import CreateDFwithDemand
+
+folder = 'optPV_data/Data/Consumo segundo/'
+
+# data path goes back to the parent folder of the repository, where optPV_data folder is
+currentpath = Path.cwd()
+datapath = currentpath.parent / folder
+
+data = CreateDFwithDemand(datapath)
+
+
+
+print()
+
---
+++ demand_data.py
@@ -0,0 +1,34 @@
+import os
+from os.path import isfile, join
+
+
+
+def getCSVfromdir(mypath):
+
+    onlyfiles = [f for f in os.listdir(mypath) if isfile(join(mypath, f))]
+
+    return onlyfiles
+
+def CreateDFwithDemand(datapath):
+
+    filenames = getCSVfromdir(datapath)
+    data = []
+    for f in filenames:
+
+        aktdata  = pd.read_excel(datapath / f, header=12)
+
+        # get index first empty row to reduce dataframe 
+
+        emptyrow = aktdata[aktdata.columns[0]].index[aktdata.Fecha.isnull()==True].tolist()[0]
+
+        #limit dataframe until empty row
+        aktdata = aktdata.loc[0:emptyrow-1,:]
+
+        if filenames.index(f) == 0:
+            data = aktdata
+
+        else:
+
+            data = data.append(aktdata)
+
+        return data
